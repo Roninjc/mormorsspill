b60d37e7cdce488af0d537259bb10a38b737f47c
Jesus Castano <jesus.castanocandela@gmail.com>
2026-07-27 15:41:49 +0200

    refactor: simplify app component structure by removing conditional landing class

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script>
 	import '../app.css';
-	import { page } from '$app/state';
 	let { children } = $props();
 </script>
 
-<div class="app" class:is-landing={page.url.pathname === '/'}>
+<div class="app">
 	{@render children()}
 </div>
