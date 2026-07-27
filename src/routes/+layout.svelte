a634310a49109b424051344df3d272b5eafaec59
Jesus Castano <jesus.castanocandela@gmail.com>
2026-07-27 15:24:36 +0200

    feat: add conditional landing class to app component for improved styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/state';
 	let { children } = $props();
 </script>
 
-<div class="app">
+<div class="app" class:is-landing={page.url.pathname === '/'}>
 	{@render children()}
 </div>
